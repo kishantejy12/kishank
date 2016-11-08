f85612cfda89da4ec8ad6e82dde23199bb119d34
KISH <KISH@172.16.57.233>
2016-11-08 12:29:43 -0500

    ji

---
+++ number/src/number/number.java
@@ -0,0 +1,58 @@
+package number;
+
+import java.util.Scanner;
+
+public class number {
+
+	public static void main(String[] args) {
+		System.out.println("entre the number");
+		Scanner sc = new Scanner(System.in);
+
+		int x = sc.nextInt();
+		int y = x;
+
+		y = 1;
+
+		for (int i = x; i > 0; i--) {
+
+			for (int j = 1; j <= i * 2; j++) {
+
+				System.out.print(" ");
+			}
+
+			for (int j = 1; j <= y - 1; j++) {
+
+				System.out.print(j + " ");
+			}
+			for (int j = y - 1; j > 1; j--) {
+
+				System.out.print(" " + j);
+			}
+			System.out.print(" " + "1");
+			System.out.println();
+
+			y++;
+		}
+
+		for (int i = 0; i < x; i++) {
+
+			for (int j = 1; j <= i * 2; j++) {
+				System.out.print(" ");
+			}
+
+			for (int j = 1; j <= y - 1; j++) {
+				System.out.print(j + " ");
+			}
+			System.out.print(" ");
+
+			for (int j = y - 1; j > 1; j--) {
+				System.out.print(j + " ");
+			}
+			System.out.print("1");
+			System.out.println();
+
+			y--;
+
+		}
+	}
+}
